6f307c92f543ff918ea7c3f205b209e1babf7094
Sebastian Müller <info@sebastian-mueller.net>
2016-09-08 16:25:42 +0200

    chore: remove old file

--- index.ts
+++
@@ -1,6 +0,0 @@
-export * from './chat';
-export * from './chat-message';
-export * from './chat-message-form';
-export * from './chat-message-list';
-export * from './navbar';
-export * from './select-room';
